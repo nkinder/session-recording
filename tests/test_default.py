edd9829e60128a38e8e05313793d5ed9dfd99645
Justin Stephenson <jstephen@redhat.com>
2019-07-10 14:56:44 -0400

    Add basic Testinfra unit tests to be called by molecule verify

--- tests/test_default.py
+++ tests/test_default.py
@@ -1,14 +1,21 @@
-import os
-
-import testinfra.utils.ansible_runner
-
-testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
-    os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
+def test_tlog_installed(host):
+    tlog = host.package("tlog")
+    assert tlog.is_installed
 
 
-def test_hosts_file(host):
-    f = host.file('/etc/hosts')
+def test_sssd_snippet_config_file(host):
+    f = host.file('/etc/sssd/conf.d/sssd-session-recording.conf')
 
     assert f.exists
     assert f.user == 'root'
     assert f.group == 'root'
+    assert f.mode == 0o600
+
+
+def test_tlog_config_file(host):
+    f = host.file('/etc/tlog/tlog-rec-session.conf')
+
+    assert f.exists
+    assert f.user == 'root'
+    assert f.group == 'root'
+    assert f.mode == 0o644
